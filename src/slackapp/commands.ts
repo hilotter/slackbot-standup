0346a86c6eadf9bc0b1d19eea9fd2075a6af099a
hilotter <hilotter3@gmail.com>
2020-01-23 12:30:33 +0900

    use workspace timezone offset

--- src/slackapp/commands.ts
+++ src/slackapp/commands.ts
@@ -1,198 +1,197 @@
 import dayjs from 'dayjs';
 import app from '~/slackapp/app';
-import { addTimezoneContext } from '~/slackapp/context';
 import { UserInfoResult } from '~/types';
 import Setting from '~/models/setting';
 import Standup from '~/models/standup';
+import Workspace from '~/models/workspace';
 
-app.command(
-  '/standup',
-  addTimezoneContext,
-  async ({ ack, payload, context }) => {
-    ack();
+app.command('/standup', async ({ ack, payload, context }) => {
+  ack();
 
-    const latestStandup = await Standup.read(payload.team_id, payload.user_id);
-    const today = dayjs()
-      .add(context.tz_offset, 'second')
-      .startOf('day');
-    const isUpdate = latestStandup && latestStandup.postDate === today.toJSON();
+  const workspace = await Workspace.read(payload.team_id);
+  const tzOffset = workspace.tzOffset || 0;
+  const latestStandup = await Standup.read(payload.team_id, payload.user_id);
+  const today = dayjs()
+    .add(tzOffset, 'second')
+    .startOf('day');
+  const isUpdate = latestStandup && latestStandup.postDate === today.toJSON();
 
-    try {
-      await app.client.views.open({
-        token: context.botToken,
-        trigger_id: payload.trigger_id,
-        view: {
-          type: 'modal',
-          callback_id: 'standup',
-          title: {
-            type: 'plain_text',
-            text: 'Daily Standup',
-            emoji: true
-          },
-          submit: {
-            type: 'plain_text',
-            text: isUpdate ? 'Update' : 'Submit',
-            emoji: true
-          },
-          close: {
-            type: 'plain_text',
-            text: 'Cancel',
-            emoji: true
-          },
-          blocks: [
-            {
-              type: 'section',
-              text: {
-                type: 'plain_text',
-                text: `:wave: Hello!\n\n${today.format(
-                  'MM/DD'
-                )} 今日の気分を教えてね :sunny:`,
-                emoji: true
-              }
-            },
-            {
-              type: 'divider'
-            },
-            {
-              type: 'input',
-              block_id: 'standup_status',
-              label: {
-                type: 'plain_text',
-                text: ':sunrise: 今日の気分はどうですか？',
-                emoji: true
-              },
-              element: {
-                type: 'static_select',
-                action_id: 'select',
-                placeholder: {
-                  type: 'plain_text',
-                  text: 'Select an item',
-                  emoji: true
-                },
-                initial_option: isUpdate
-                  ? {
-                      text: {
-                        type: 'plain_text',
-                        text: latestStandup.status,
-                        emoji: true
-                      },
-                      value: latestStandup.status
-                    }
-                  : undefined,
-                options: [
-                  {
-                    text: {
-                      type: 'plain_text',
-                      text: '最高',
-                      emoji: true
-                    },
-                    value: '最高'
-                  },
-                  {
-                    text: {
-                      type: 'plain_text',
-                      text: 'いいかんじ',
-                      emoji: true
-                    },
-                    value: 'いいかんじ'
-                  },
-                  {
-                    text: {
-                      type: 'plain_text',
-                      text: 'ちょっとわるい',
-                      emoji: true
-                    },
-                    value: 'ちょっとわるい'
-                  },
-                  {
-                    text: {
-                      type: 'plain_text',
-                      text: 'きびしい',
-                      emoji: true
-                    },
-                    value: 'きびしい'
-                  },
-                  {
-                    text: {
-                      type: 'plain_text',
-                      text: 'もうダメ、だれか助けて',
-                      emoji: true
-                    },
-                    value: 'もうダメ、だれか助けて'
-                  }
-                ]
-              }
-            },
-            {
-              type: 'input',
-              block_id: 'standup_last_time_todo',
-              label: {
-                type: 'plain_text',
-                text: ':bee: 前回はなにをしましたか？',
-                emoji: true
-              },
-              element: {
-                type: 'plain_text_input',
-                action_id: 'input',
-                multiline: true,
-                initial_value: isUpdate ? latestStandup.lastTimeTodo : undefined
-              }
-            },
-            {
-              type: 'input',
-              block_id: 'standup_today_todo',
-              label: {
-                type: 'plain_text',
-                text: ':books: 今日はなにをしますか？',
-                emoji: true
-              },
-              element: {
-                type: 'plain_text_input',
-                action_id: 'input',
-                multiline: true,
-                initial_value: isUpdate ? latestStandup.todayTodo : undefined
-              }
-            },
-            {
-              type: 'input',
-              block_id: 'standup_trouble',
-              label: {
-                type: 'plain_text',
-                text: ':tractor: 困りごと、悩みごとはありますか？',
-                emoji: true
-              },
-              element: {
-                type: 'plain_text_input',
-                action_id: 'input',
-                multiline: true,
-                initial_value: isUpdate ? latestStandup.trouble : undefined
-              },
-              optional: true
-            },
-            {
-              type: 'input',
-              block_id: 'standup_good_point',
-              label: {
-                type: 'plain_text',
-                text: ':bulb: 最近のよかったことを教えてほしいです',
-                emoji: true
-              },
-              element: {
-                type: 'plain_text_input',
-                action_id: 'input',
-                multiline: true,
-                initial_value: isUpdate ? latestStandup.goodPoint : undefined
-              },
-              optional: true
+  try {
+    await app.client.views.open({
+      token: context.botToken,
+      trigger_id: payload.trigger_id,
+      view: {
+        type: 'modal',
+        callback_id: 'standup',
+        title: {
+          type: 'plain_text',
+          text: 'Daily Standup',
+          emoji: true
+        },
+        submit: {
+          type: 'plain_text',
+          text: isUpdate ? 'Update' : 'Submit',
+          emoji: true
+        },
+        close: {
+          type: 'plain_text',
+          text: 'Cancel',
+          emoji: true
+        },
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'plain_text',
+              text: `:wave: Hello!\n\n${today.format(
+                'MM/DD'
+              )} 今日の気分を教えてね :sunny:`,
+              emoji: true
             }
-          ]
-        }
-      });
-    } catch (error) {
-      console.error(error);
-    }
+          },
+          {
+            type: 'divider'
+          },
+          {
+            type: 'input',
+            block_id: 'standup_status',
+            label: {
+              type: 'plain_text',
+              text: ':sunrise: 今日の気分はどうですか？',
+              emoji: true
+            },
+            element: {
+              type: 'static_select',
+              action_id: 'select',
+              placeholder: {
+                type: 'plain_text',
+                text: 'Select an item',
+                emoji: true
+              },
+              initial_option: isUpdate
+                ? {
+                    text: {
+                      type: 'plain_text',
+                      text: latestStandup.status,
+                      emoji: true
+                    },
+                    value: latestStandup.status
+                  }
+                : undefined,
+              options: [
+                {
+                  text: {
+                    type: 'plain_text',
+                    text: '最高',
+                    emoji: true
+                  },
+                  value: '最高'
+                },
+                {
+                  text: {
+                    type: 'plain_text',
+                    text: 'いいかんじ',
+                    emoji: true
+                  },
+                  value: 'いいかんじ'
+                },
+                {
+                  text: {
+                    type: 'plain_text',
+                    text: 'ちょっとわるい',
+                    emoji: true
+                  },
+                  value: 'ちょっとわるい'
+                },
+                {
+                  text: {
+                    type: 'plain_text',
+                    text: 'きびしい',
+                    emoji: true
+                  },
+                  value: 'きびしい'
+                },
+                {
+                  text: {
+                    type: 'plain_text',
+                    text: 'もうダメ、だれか助けて',
+                    emoji: true
+                  },
+                  value: 'もうダメ、だれか助けて'
+                }
+              ]
+            }
+          },
+          {
+            type: 'input',
+            block_id: 'standup_last_time_todo',
+            label: {
+              type: 'plain_text',
+              text: ':bee: 前回はなにをしましたか？',
+              emoji: true
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'input',
+              multiline: true,
+              initial_value: isUpdate ? latestStandup.lastTimeTodo : undefined
+            }
+          },
+          {
+            type: 'input',
+            block_id: 'standup_today_todo',
+            label: {
+              type: 'plain_text',
+              text: ':books: 今日はなにをしますか？',
+              emoji: true
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'input',
+              multiline: true,
+              initial_value: isUpdate ? latestStandup.todayTodo : undefined
+            }
+          },
+          {
+            type: 'input',
+            block_id: 'standup_trouble',
+            label: {
+              type: 'plain_text',
+              text: ':tractor: 困りごと、悩みごとはありますか？',
+              emoji: true
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'input',
+              multiline: true,
+              initial_value: isUpdate ? latestStandup.trouble : undefined
+            },
+            optional: true
+          },
+          {
+            type: 'input',
+            block_id: 'standup_good_point',
+            label: {
+              type: 'plain_text',
+              text: ':bulb: 最近のよかったことを教えてほしいです',
+              emoji: true
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'input',
+              multiline: true,
+              initial_value: isUpdate ? latestStandup.goodPoint : undefined
+            },
+            optional: true
+          }
+        ]
+      }
+    });
+  } catch (error) {
+    console.error(error.data.response_metadata);
+    console.error(error);
   }
-);
+});
 
 app.command('/standup-setting', async ({ ack, payload, context, say }) => {
   ack();
