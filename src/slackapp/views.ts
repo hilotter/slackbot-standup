c57ffcf1fef701f6aefd88b66db7291fa4e0e00d
hilotter <hilotter3@gmail.com>
2021-07-07 09:37:14 +0900

    add excludeFromIndexes to standup datastore

--- src/slackapp/views.ts
+++ src/slackapp/views.ts
@@ -11,20 +11,18 @@ app.view('standup', async ({ ack, body, view, context }) => {
   const teamId = view.team_id;
   const workspace = await Workspace.read(teamId);
   const tzOffset = workspace.tzOffset || 0;
-  const postDate = dayjs()
-    .add(tzOffset, 'second')
-    .startOf('day')
-    .toJSON();
+  const postDate = dayjs().add(tzOffset, 'second').startOf('day').toJSON();
 
   const values = view['state']['values'];
-  const status = values['standup_status']['select'].selected_option.value;
-  const lastTimeTodo = values['standup_last_time_todo']['input'].value;
-  const todayTodo = values['standup_today_todo']['input'].value;
-  const trouble = values['standup_trouble']['input'].value;
-  const goodPoint = values['standup_good_point']['input'].value;
+  const status =
+    values['standup_status']['select'].selected_option?.value ?? '';
+  const lastTimeTodo = values['standup_last_time_todo']['input'].value ?? '';
+  const todayTodo = values['standup_today_todo']['input'].value ?? '';
+  const trouble = values['standup_trouble']['input'].value ?? null;
+  const goodPoint = values['standup_good_point']['input'].value ?? null;
   const workPlace =
-    values['standup_work_place']['select'].selected_option?.value;
-  const information = values['standup_information']['input'].value;
+    values['standup_work_place']['select'].selected_option?.value ?? null;
+  const information = values['standup_information']['input'].value ?? null;
   const userId = body.user.id;
   const userName = body.user.name;
   const isUpdate = view.submit!.text === 'Update';
@@ -36,7 +34,7 @@ app.view('standup', async ({ ack, body, view, context }) => {
         token: context.botToken,
         user: userId,
         channel: userId,
-        text: 'setting not found. please input "/standup-setting" in advance'
+        text: 'setting not found. please input "/standup-setting" in advance',
       });
     } catch (error) {
       console.error(error);
@@ -46,7 +44,7 @@ app.view('standup', async ({ ack, body, view, context }) => {
 
   const userProfile = (await app.client.users.profile.get({
     token: context.botToken,
-    user: userId
+    user: userId,
   })) as UserProfileResult;
 
   const headBlockTexts = ['*今日の気分はどうですか？*', `${status}\n`];
@@ -54,7 +52,7 @@ app.view('standup', async ({ ack, body, view, context }) => {
     '*前回はなにをしましたか？*',
     `${lastTimeTodo}\n`,
     '*今日はなにをしますか？*',
-    `${todayTodo}\n`
+    `${todayTodo}\n`,
   ];
   if (trouble) {
     bodyBlockTexts.push('*困りごと、悩みごとはありますか？*');
@@ -79,7 +77,7 @@ app.view('standup', async ({ ack, body, view, context }) => {
     text: '',
     blocks: [
       {
-        type: 'divider'
+        type: 'divider',
       },
       {
         type: 'context',
@@ -87,29 +85,29 @@ app.view('standup', async ({ ack, body, view, context }) => {
           {
             type: 'image',
             image_url: userProfile.profile.image_192,
-            alt_text: userName
+            alt_text: userName,
           },
           {
             type: 'mrkdwn',
-            text: `${userName}'s daily standup`
-          }
-        ]
+            text: `${userName}'s daily standup`,
+          },
+        ],
       },
       {
         type: 'section',
         text: {
           type: 'mrkdwn',
-          text: headBlockTexts.join('\n')
-        }
+          text: headBlockTexts.join('\n'),
+        },
       },
       {
         type: 'section',
         text: {
           type: 'mrkdwn',
-          text: bodyBlockTexts.join('\n')
-        }
-      }
-    ]
+          text: bodyBlockTexts.join('\n'),
+        },
+      },
+    ],
   };
 
   try {
@@ -118,7 +116,7 @@ app.view('standup', async ({ ack, body, view, context }) => {
       const latestStandup = await Standup.read(teamId, userId);
       const updateMessageArguments = {
         ...messageArguments,
-        ...{ ts: latestStandup.ts }
+        ...{ ts: latestStandup.ts },
       };
 
       result = (await app.client.chat.update(
@@ -141,15 +139,25 @@ app.view('standup', async ({ ack, body, view, context }) => {
       goodPoint,
       workPlace,
       information,
-      ts
+      ts,
     };
     await Standup.add({
       teamId,
       userId,
       userName,
-      standupInfo
+      standupInfo,
     });
   } catch (error) {
+    await app.client.chat
+      .postEphemeral({
+        token: context.botToken,
+        user: userId,
+        channel: userId,
+        text: `Sorry, an error has occurred. ${error.data.error}`,
+      })
+      .catch((err) => {
+        console.error(err);
+      });
     console.error(error);
   }
 });
@@ -158,15 +166,16 @@ app.view('standup_setting', async ({ ack, body, view, context }) => {
   await ack();
 
   const teamId: string = view.team_id;
-  const broadcastChannel: string =
+  const broadcastChannel =
     view['state']['values']['setting_broadcast_channel_input']['channel']
-      .selected_channel;
-  const reminderText: string =
-    view['state']['values']['setting_reminder_input']['reminder_input'].value;
+      .selected_channel ?? '';
+  const reminderText =
+    view['state']['values']['setting_reminder_input']['reminder_input'].value ??
+    '';
 
   const settingInfo = {
     broadcastChannel,
-    reminderText
+    reminderText,
   };
   const user = body.user.id;
 
@@ -183,7 +192,7 @@ app.view('standup_setting', async ({ ack, body, view, context }) => {
     await app.client.chat.postMessage({
       token: context.botToken,
       channel: user,
-      text: msg
+      text: msg,
     });
   } catch (error) {
     console.error(error);
